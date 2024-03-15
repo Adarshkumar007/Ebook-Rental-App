d2363780bdbfcde88377b36fe912f39ddfc9b6cd
K-N-Ganesh <knganesh45@gamil.com>
2024-03-15 22:14:55 +0530

    add index ,App comp

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Ebook Rental App</h1>
+      <p>This is a paragraph of text in the app.</p>
+    </div>
+  );
+}
+
+export default App;
