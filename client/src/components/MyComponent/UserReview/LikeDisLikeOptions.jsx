c224203e72810eaf97e36a422b04d43228723013
Adarsh Kumar <adarshkumarnitte@gmail.com>
2024-04-30 13:57:23 +0530

    User individual comments on Navbar

---
+++ client/src/components/MyComponent/UserReview/LikeDisLikeOptions.jsx
@@ -0,0 +1,13 @@
+ import EditDelete from "./EditDelete";
+ import MyLikeDislike from "./MyLikeDislike";
+ const LikeDislikeOptions=()=>{
+    return(
+        <div className="LikeDislikeOptions">
+            <EditDelete/>
+            <MyLikeDislike />
+        </div>
+
+    )
+ }
+
+ export default LikeDislikeOptions;
