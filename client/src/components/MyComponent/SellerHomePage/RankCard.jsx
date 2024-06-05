54b46d7dec4d16fe754fbe99c81881122714e152
Adarsh Kumar <adarshkumarnitte@gmail.com>
2024-06-05 09:24:43 +0530

    Rank Load

--- client/src/components/MyComponent/SellerHomePage/RankCard.jsx
+++ client/src/components/MyComponent/SellerHomePage/RankCard.jsx
@@ -1,38 +1,50 @@
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { url } from "../../../url";
-import { useEffect, useState } from "react";
+import LoadingSpinner from "../LoadingSpinner";
 
-const RankCard = ({rank, book}) => {
-  const [bookinfo ,setBookinfo] = useState([]);
-  useEffect(()=>{
+const RankCard = ({ rank, book }) => {
+  const [bookinfo, setBookinfo] = useState(null);
+  const [fetching, setFetching] = useState(true);
+
+  useEffect(() => {
     const fetchData = async () => {
-    try {
-      const response = await axios.get(url+`/bookinfo?bookId=${book._id}`);
-    setBookinfo(response.data);
-    console.log("ssd",response);
-  } catch (error) {
-      
-    } 
-  }
-  fetchData();
-},[]);
+      try {
+        const response = await axios.get(`${url}/bookinfo?bookId=${book._id}`);
+        setBookinfo(response.data);
+        setFetching(false); // Set fetching to false after data is fetched
+        console.log("ssd", response);
+      } catch (error) {
+        setFetching(false); // Set fetching to false in case of an error
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [book._id]);
+
   return (
     <div className="RankCard SubContainer">
-        <div className="RankNo My-Title-Style">
+      {fetching ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <div className="RankNo My-Title-Style">
             #{rank}
-        </div>
-      <div className="RankCardImage SubContainer">
-        <img src={bookinfo.imageSrc} alt="book" className="rankimage" />
-      </div>
-      <div className="RankCardInfo">
-        <div className="Rank-Book-Name">{bookinfo.title}</div>
-        <div className="BookStats">
-          <div>Total Earning: ₹{book.totalAmount/100}</div>
-          <div>Total Subscribes: {book.count}</div>
-        </div>
-      </div>
-      
+          </div>
+          <div className="RankCardImage SubContainer">
+            <img src={bookinfo?.imageSrc} alt="book" className="rankimage" />
+          </div>
+          <div className="RankCardInfo">
+            <div className="Rank-Book-Name">{bookinfo?.title}</div>
+            <div className="BookStats">
+              <div>Total Earning: ₹{book.totalAmount / 100}</div>
+              <div>Total Subscribes: {book.count}</div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
+
 export default RankCard;
