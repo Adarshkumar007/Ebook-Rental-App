c3c063a3ffdcbc2ec278744ca28d8e1239134481
Adarsh Kumar <adarshkumarnitte@gmail.com>
2024-06-21 17:50:01 +0530

    Verified Badge

--- client/src/components/MyComponent/SellerHomePage/BankAccount.jsx
+++ client/src/components/MyComponent/SellerHomePage/BankAccount.jsx
@@ -1,18 +1,32 @@
 import img1 from "../../images/officialphoto.jpg";
 import img2 from "../../images/NoImage.png";
 import AccountDetails from "./AccountDetails";
+import SellerVerifyBadge from "./SellerVerifyBadge";
+import { useState } from "react";
 
-const BankAccount = ({editable,setWarning}) => {
+const BankAccount = ({ editable, setWarning }) => {
   const image = img1;
+
+  // const [status, setStatus] = useState("Account Verified");
+  const [status, setStatus] = useState("Account Verifying");
+  // const [status, setStatus] = useState("Account Blocked");
+
+
   return (
     <div className="BankAccount">
-      <img
-        src={image}
-        alt="Your Photo"
-        className="BankProfileImage SubContainer"
-      />
+      <div className="sellerphoto-status-container">
+        <img
+          src={image}
+          alt="Your Photo"
+          className="BankProfileImage SubContainer"
+        />
+        <div className="verification-status">
+          <SellerVerifyBadge status={status}/>
+        </div>
+      </div>
+
       {/* <AccountDetails editable={editable}/> */}
-      <AccountDetails editable={editable} setWarning={setWarning}/>
+      <AccountDetails editable={editable} setWarning={setWarning} />
     </div>
   );
 };
